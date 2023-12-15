5a7c6c8456d389ebb0ab7f55c9b7caf1464b1914
Wudado <146415959+Wudado@users.noreply.github.com>
2023-12-15 19:13:28 +0300

    Update Lab2.cpp

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,31 +1,56 @@
-﻿#include <iostream>
-#include <cmath>
-#include <vector>
-#include "Source.h"
-
-using namespace std;
+#include "Newton.h"
+#include "Gauss.h"
+#include <windows.h>
 
 int main() {
-    setlocale(LC_ALL, "RUS");
-    double x1, x2, E1, E2;
-    int NIT;
+    setlocale(LC_ALL, "rus");
+    system("cls");
 
-    cout << "Введите начальное приближение x1: ";
-    cin >> x1;
+    vector<vector<double>> answers1;
+    vector<vector<double>> answers2;
+    int size, size1;
+    bool same;
+    int statsEvery = 5;
+    double epsilon1 = 1e-9;
+    double epsilon2 = 1e-6;
+    int NIT = 100;
 
-    cout << "Введите начальное приближение x2: ";
-    cin >> x2;
+    answers1.push_back(calculateNewton({ 3, 2 }, epsilon1, epsilon2, NIT, 0, true, statsEvery));
+    answers1.push_back(calculateNewton({ 3, 2 }, epsilon1, epsilon2, NIT, 0.5, true, statsEvery));
+    answers1.push_back(calculateNewton({ 3, 2 }, epsilon1, epsilon2, NIT, 0.1, true, statsEvery));
+    answers1.push_back(calculateNewton({ 3, 2 }, epsilon1, epsilon2, NIT, 0.01, true, statsEvery));
 
-    cout << "Введите параметр E1: ";
-    cin >> E1;
+    size = answers1.size();
+    size1 = answers1[0].size();
+    same = true;
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size1; ++j) {
+            if (abs(answers1[i][j] - answers1[i + 1][j]) > 1e-6) {
+                same = false;
+                break;
+            }
+        }
+    }
+    if (same) { cout << "Ответы совпадают.\n\n\n\n\n"; }
+    else { cout << "Ответы не совпадают.\n\n\n\n\n"; }
 
-    cout << "Введите параметр E2: ";
-    cin >> E2;
 
-    cout << "Введите предельное число итераций NIT: ";
-    cin >> NIT;
+    answers2.push_back(calculateNewton({ 3, -2 }, epsilon1, 1e-6, NIT, 0, true, statsEvery));
+    answers2.push_back(calculateNewton({ 3, -2 }, epsilon1, 1e-6, NIT, 0.5, true, statsEvery));
+    answers2.push_back(calculateNewton({ 3, -2 }, epsilon1, 1e-6, NIT, 0.1, true, statsEvery));
+    answers2.push_back(calculateNewton({ 3, -2 }, epsilon1, 1e-6, NIT, 0.01, true, statsEvery));
 
-    newtonMethod(x1, x2, E1, E2, NIT);
-
-    return 0;
+    size = answers2.size();
+    size1 = answers2[0].size();
+    same = true;
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size1; ++j) {
+            if (abs(answers2[i][j] - answers2[i + 1][j]) > 1e-6) {
+                same = false;
+                break;
+            }
+        }
+    }
+    if (same) { cout << "Ответы совпадают.\n\n\n\n\n"; }
+    else { cout << "Ответы не совпадают.\n\n\n\n\n"; }
 }
